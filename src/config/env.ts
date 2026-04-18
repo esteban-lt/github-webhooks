66d39a3dd930e9b973c3c0a5b6f2231e47cb6531
esteban-lt <ledezmatenaesteban@gmail.com>
2026-04-17 22:53:25 -0600

    add WEBHOOK_SECRET environment variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,5 @@ import { get } from 'env-var';
 export const env = {
   PORT: get('PORT').required().asPortNumber(),
   DISCORD_WEBHOOK_URL: get('DISCORD_WEBHOOK_URL').required().asString(),
+  WEBHOOK_SECRET: get('WEBHOOK_SECRET').required().asString(),
 }
